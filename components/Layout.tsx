acb1bda64c07e19e01b84b37daa7932ecc38ee58
Lailo <lailo@helio.exchange>
2020-03-15 12:03:43 +0100

    adds custom 404 page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import Head from 'next/head'
 import Header from './Header'
+import clsx from 'clsx'
 
 interface LayoutProps {
   title?: string
+  centered?: boolean
 }
 
-const Layout: React.FC<LayoutProps> = ({ title, children }) => (
+const Layout: React.FC<LayoutProps> = ({ title, centered = false, children }) => (
   <>
     <Head>
       <title>{`Blog ${title ? ` | ${title}` : ''}`}</title>
     </Head>
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="container px-3 max-w-3xl flex-grow">{children}</main>
+      <main
+        className={clsx('container px-3 max-w-3xl', 'flex-grow', {
+          'flex flex-col items-center justify-center': centered,
+        })}
+      >
+        {children}
+      </main>
       <footer className="text-xs text-center text-gray-600 py-4  mt-10">
         Made with ♥ by <a href="https://www.twitter.com/lailo_ch">@lailo_ch</a>
       </footer>
---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Error = () => (
+  <Layout title="Page Not Found" centered>
+    <h1 className="text-3xl font-black mb-2">Not found</h1>
+    <p className="text-center text-lg font-light">Couldn't find wha you're looking for.</p>
+  </Layout>
+)
+
+export default Error
