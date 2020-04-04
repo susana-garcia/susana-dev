af54f201d3da2c5a955177391cdf98c71298e2ff
Lailo <lailo@helio.exchange>
2020-04-04 18:56:02 +0200

    adds sitemap links to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,32 +5,77 @@ import dynamic from 'next/dynamic'
 import Container from 'components/layout/Container'
 import clsx from 'clsx'
 import { Routes } from 'utils/routes'
+import { useRouter } from 'next/router'
 
 const DarkModeToggle = dynamic(() => import('components/DarkModeToggle'), {
   ssr: false,
 })
 
-const Header: React.FC = ({ children }) => (
-  <header
-    className={clsx({
-      'bg-white border-b border-gray-300': children,
-      'dark:bg-gray-900 dark:border-gray-800': children,
-    })}
-  >
-    <Container size="fluid" grid className="h-12 grid-cols-2 py-0 items-center">
-      <div>
-        <NextLink {...Routes.home()}>
-          <Link title="Back home" className="font-black text-black dark:text-white">
-            Lailo
-          </Link>
-        </NextLink>
-      </div>
-      <div className="text-right">
-        <DarkModeToggle />
-      </div>
-    </Container>
-    {children && <Container>{children}</Container>}
-  </header>
-)
+const Header: React.FC = ({ children }) => {
+  const { pathname } = useRouter()
+
+  return (
+    <header
+      className={clsx({
+        'bg-white border-b border-gray-300': children,
+        'dark:bg-gray-900 dark:border-gray-800': children,
+      })}
+    >
+      <Container size="fluid" grid className="h-12 py-0">
+        <div className="flex items-center">
+          <div>
+            <NextLink {...Routes.home()}>
+              <Link title="Back home" className={clsx('font-black text-black dark:text-white')}>
+                Lailo
+              </Link>
+            </NextLink>
+          </div>
+          <ul className="flex mr-auto">
+            <li className="mr-1">
+              <NextLink {...Routes.articles()} passHref>
+                <Link
+                  title="All Articles"
+                  className={clsx('text-sm', {
+                    'bg-primary text-white': pathname === Routes.articles().as,
+                  })}
+                >
+                  Articles
+                </Link>
+              </NextLink>
+            </li>
+            <li className="mr-1">
+              <NextLink {...Routes.tips()} passHref>
+                <Link
+                  title="Quick Tags"
+                  className={clsx('text-sm', {
+                    'bg-primary text-white': pathname === Routes.tips().as,
+                  })}
+                >
+                  Tips
+                </Link>
+              </NextLink>
+            </li>
+            <li className="mr-1">
+              <NextLink {...Routes.tags()} passHref>
+                <Link
+                  title="Quick Tags"
+                  className={clsx('text-sm', {
+                    'bg-primary text-white': pathname === Routes.tags().as,
+                  })}
+                >
+                  Tags
+                </Link>
+              </NextLink>
+            </li>
+          </ul>
+          <div>
+            <DarkModeToggle />
+          </div>
+        </div>
+      </Container>
+      {children && <Container>{children}</Container>}
+    </header>
+  )
+}
 
 export default Header
