1dd0541fc118dbaf52351d2cff6e316a5daae3fd
Lailo <lailo@helio.exchange>
2020-04-05 20:05:37 +0200

    adds undraw illustration to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Layout from 'components/layout/Layout'
 import { NextSeo } from 'next-seo'
+import NotFoundSVG from 'assets/svg/undraw-empty.svg'
 
 const Error = () => (
   <>
     <NextSeo title="Page Not Found" description="Couldn't find wha you're looking for." />
     <Layout centered>
+      <NotFoundSVG className="text-primary w-64 h-64" />
       <h1 className="text-3xl font-black mb-2">Not found</h1>
-      <p className="text-center text-lg font-light">Couldn't find wha you're looking for.</p>
+      <p className="text-center text-lg font-light">Couldn't find what you're looking for.</p>
     </Layout>
   </>
 )
