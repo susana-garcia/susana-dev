b48054bdb8aa522978850ac2548e6f7da87d3458
Lailo <lailo@helio.exchange>
2020-04-17 20:08:56 +0200

    adds custom code block styles for tips

--- src/components/layout/Markdown.tsx
+++ src/components/layout/Markdown.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import marked from 'marked'
 import hljs from 'highlight.js'
+import { ContentType } from 'utils/contents'
 
 interface MarkdownProps {
   content: string
+  contentType?: ContentType
 }
 
-const Markdown: React.FC<MarkdownProps> = ({ content }) => (
+const Markdown: React.FC<MarkdownProps> = ({ content, contentType = 'article' }) => (
   <div
     className="markdown"
+    id={contentType}
     dangerouslySetInnerHTML={{
       __html: marked(content, {
         highlight: (code, lang) => hljs.highlight(lang, code).value,
